d9d187ba01127ca1fb00fc930df76fdfce3816d4
Arpit <arpit.mishra.out@gmail.com>
2024-04-07 10:51:12 +0530

    Revise some fundamental Javascript and solve some basic problems on it.

---
+++ Section 12 - JavaScript/js/script.js
@@ -0,0 +1,40 @@
+`use strict`;
+
+// alert("Hello, World!");
+
+// let myName = "Arpit";
+// alert(myName);
+
+// let name = "Arpit";
+// let msg = "Good Morning!";
+// alert(msg + " " + name);
+
+// const tweetLengthAllowed = 140;
+// const tweet = prompt(`Enter your tweet`);
+// console.log(
+//   `You have written ${tweet.length} characters, you have ${
+//     tweetLengthAllowed - tweet.length
+//   } left`
+// );
+
+// const tweet = prompt("Enter your tweet: ");
+// const tweetLength = tweet.length;
+// const tweetLengthAllowed = 10;
+
+// if (tweetLengthAllowed < tweetLength) {
+//   alert(`You have crossed the limit, your tweet is cutted to ${tweetLengthAllowed} chars only
+//   Your tweet: ${tweet.slice(0, tweetLengthAllowed)}`);
+// } else {
+//   alert(`Your tweet: ${tweet}`);
+// }
+
+// const firstName = prompt("Enter your name: ").toLowerCase();
+
+// console.log("Hello " + firstName[0].toUpperCase() + firstName.slice(1));
+
+// dog to human age converter
+
+// const dogAge = Number(prompt("Enter age: "));
+
+// const humanAge = (dogAge - 2) * 4 + 21;
+// console.log(humanAge);
