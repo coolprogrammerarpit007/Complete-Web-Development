861b8b540a246340d09eb9a38e809b4bf4cfa27c
Arpit <arpit.mishra.out@gmail.com>
2024-04-12 07:44:50 +0530

    learn about objects, classes in JS. create keyboard event to the drum kit project and added animation to it. And Introduced to callback also.

--- Section 16 - Advance DOM/index.js
+++ Section 16 - Advance DOM/index.js
@@ -4,6 +4,8 @@
 
 const buttons = document.querySelectorAll(".drum");
 
+// state variables
+
 // storing audio into array
 
 const sounds = [
@@ -31,25 +33,56 @@ for (let i = 0; i < buttons.length; i++) {
   });
 }
 
-// ************************\
+// Adding event listener for keyboard events to the drum kit.
 
-// calculator challenge
+// function to play sound when key is pressed
 
-// const add = function (num1, num2) {
-//   return num1 + num2;
-// };
-// const sub = function (num1, num2) {
-//   return num1 - num2;
-// };
-// const mul = function (num1, num2) {
-//   return num1 * num2;
-// };
-// const divide = function (num1, num2) {
-//   return num1 / num2;
-// };
+const playSound = function (keyboardBtn) {
+  switch (keyboardBtn) {
+    case "w":
+      let tom1 = new Audio("./sounds/crash.mp3");
+      tom1.play();
+      break;
+    case "a":
+      let tom2 = new Audio("./sounds/kick-bass.mp3");
+      tom2.play();
+      break;
+    case "s":
+      let tom3 = new Audio("./sounds/snare.mp3");
+      tom3.play();
+      break;
+    case "d":
+      let tom4 = new Audio("./sounds/tom-1.mp3");
+      tom4.play();
+      break;
+    case "j":
+      let tom5 = new Audio("./sounds/tom-2.mp3");
+      tom5.play();
+      break;
+    case "k":
+      let tom6 = new Audio("./sounds/tom-3.mp3");
+      tom6.play();
+      break;
+    case "l":
+      let tom7 = new Audio("./sounds/tom-4.mp3");
+      tom7.play();
+      break;
+  }
+};
 
-// const calculate = function (num1, num2, op) {
-//   return op(num1, num2);
-// };
+document.addEventListener("keydown", function (e) {
+  let keyboardBtn = e.key;
+  playSound(keyboardBtn);
+  buttonAnimation(keyboardBtn);
+});
 
-// console.log(calculate(15, 15, divide));
+// Adding Animation to the website
+
+const buttonAnimation = function (key) {
+  let activeBtn = document.querySelector(`.${key}`);
+  activeBtn.classList.add("pressed");
+
+  setTimeout(() => {
+    activeBtn.classList.remove("pressed");
+  }, 100);
+};
---
+++ Section 16 - Advance DOM/oops.js
@@ -0,0 +1,60 @@
+// ************************\
+// calculator challenge
+// const add = function (num1, num2) {
+//   return num1 + num2;
+// };
+// const sub = function (num1, num2) {
+//   return num1 - num2;
+// };
+// const mul = function (num1, num2) {
+//   return num1 * num2;
+// };
+// const divide = function (num1, num2) {
+//   return num1 / num2;
+// };
+// const calculate = function (num1, num2, op) {
+//   return op(num1, num2);
+// };
+// console.log(calculate(15, 15, divide));
+
+// *************************************************************************
+// *************************************************************************
+// working with the Objects
+// const houseMaid = {
+//   name: "Sara",
+//   age: 23,
+//   experience: 2,
+//   tasks: ["Cleaing", "Making Food", "Laundry"],
+// };
+// console.log(houseMaid);
+
+// *********************************************************************
+// *********************************************************************
+// class  in JS
+
+// class BELLBOY {
+//   // Attributes in class
+//   constructor(name, age, experience, languages, tasks) {
+//     this.name = name;
+//     this.age = age;
+//     this.experience = experience;
+//     this.languages = languages;
+//     this.tasks = tasks;
+//   }
+
+//   // Methods in class
+
+//   cleaning() {
+//     alert(`Cleaning is in progress..`);
+//   }
+// }
+
+// const bellBoy1 = new BELLBOY(
+//   "John",
+//   27,
+//   5,
+//   ["English", "French"],
+//   ["Welcoming and escorting guests", "Room Service", "Food Service"]
+// );
+// console.log(bellBoy1);
+// bellBoy1.cleaning();
