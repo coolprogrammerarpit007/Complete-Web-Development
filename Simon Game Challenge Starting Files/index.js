510a83436bc69af9e952dd2d242d3087462df71d
Arpit <arpit.mishra.out@gmail.com>
2024-04-14 21:57:11 +0530

    Simon Game Challenge Completed

--- Simon Game Challenge Starting Files/index.js
+++ Simon Game Challenge Starting Files/index.js
@@ -1,71 +1,163 @@
 `use strict`;
-// state vaariables
-let level = 0;
-let isStarted = false;
-let randomChosenColor;
-let randomNumber;
-// *************************
-const sounds = ["blue.mp3", "green.mp3", "red.mp3", "yellow.mp3"];
-const userClickedPattern = [];
-const gamePattern = [];
+
+// Array to store sounds
+const sound = ["blue.mp3", "green.mp3", "red.mp3", "yellow.mp3"];
+
+// Array to store color sequence
 const buttonColors = ["red", "blue", "green", "yellow"];
 
-const nextSequence = () => {
+// Array to store game pattern generated randomlly
+let gamePattern = [];
+
+// Array to store user clicked patterns
+let userClickedPattern = [];
+
+// State Variables
+let hasGameStarted = false;
+let gameLevel = 0;
+
+// *****************************
+// *****************************
+// function to generate a random sequence
+
+const nextSequence = function () {
+  // reset userClickedPattern
+  userClickedPattern = [];
+  console.log("Next Sequence --");
+  console.log(userClickedPattern);
+  // Generate a random Number b/w 0 and 3
+
   const randomNumber = Math.floor(Math.random() * 4);
-  $("h1").text(`Level ${level}`);
-  level++;
-  randomChosenColor = buttonColors[randomNumber];
+  // generate random color
+  const randomChosenColor = buttonColors[randomNumber];
+  // storing random color to game patter
+
   gamePattern.push(randomChosenColor);
-  flashButton();
+  console.log(gamePattern);
+
+  // Calling function to flashColor
+  flashColor(randomChosenColor);
+
+  // Calling Animate Press
   animatePress(randomChosenColor);
+
+  // calling playSound function
+  playSound(sound[randomNumber]);
+
+  // Update the title
+  $("h1").text(`Level ${gameLevel}`);
+
+  // Increse the level
+  gameLevel++;
 };
 
-// flash out selected button
-const flashButton = function () {
-  $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-};
+// Automating Play
 
-// play an sound on random selected button
+$(document).keydown(function (e) {
+  if (!hasGameStarted) {
+    // Update state variable to not started to started.
+    hasGameStarted = true;
+    gameLevel = 0;
+    $("h1").text(`Level ${gameLevel}`);
 
-const playSound = function () {
-  let sound = new Audio(`./sounds/${sounds[randomNumber]}`);
-  sound.play();
-};
-
-// User Interaction
-
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Enter") {
-    if (!isStarted) {
-      randomNumber = nextSequence();
-      isStarted = true;
-      playSound();
-    }
+    // Everytime "KEY" pressed a new pattern will be generated.
+    nextSequence();
   }
 });
 
-// user playSound
+// restart the game
+function restartGame() {
+  // resetting value
+  level = 0;
+  gamePattern = [];
+  hasGameStarted = false;
+  $("h1").text("Press A Key to Start");
+}
 
-const userSound = (name) => {
-  let sound = new Audio(`./sounds/${name}.mp3`);
-  sound.play();
-};
+// function to flash Color
 
-// Adding Animations
+function flashColor(colorId) {
+  $(`#${colorId}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+}
 
-const animatePress = (currentColor) => {
+// function to playSound
+function playSound(audio) {
+  const beat = new Audio(`./sounds/${audio}`);
+  beat.play();
+}
+
+// function to animate Color
+function animatePress(currentColor) {
+  // Add press class to button
   $(`#${currentColor}`).addClass("pressed");
+
+  //  remove press class after 100s
   setTimeout(() => {
     $(`#${currentColor}`).removeClass("pressed");
-  }, 100);
-};
+  }, "100");
+}
 
-// Trigger a action when any button is clicked
+// Check Answer
+
+function checkAnswer(currentLevel) {
+  // check if both user and computer selected same answer
+  console.log("current Level", currentLevel);
+  let userSelected = userClickedPattern[currentLevel];
+  let computerSelected = gamePattern[currentLevel];
+  console.log(userClickedPattern);
+  console.log(gamePattern);
+  console.log("user selected color", userSelected);
+  console.log("computer selected", computerSelected);
+
+  // // Check if most recent user answer is same as game pattern
+  if (userSelected === computerSelected) {
+    console.log("Sucess");
+    //   // If the user got the most recent answer right in step 3, then check that they have finished their sequence
+    if (userClickedPattern.length === gamePattern.length) {
+      //     // Call nextSequence() after a 1000 millisecond delay.
+      setTimeout(() => {
+        nextSequence();
+      }, "1000");
+    }
+  } else {
+    // if wrong answer play this audio
+    console.log("wrong");
+    const lose = new Audio(`./sounds/wrong.mp3`);
+    lose.play();
+
+    // add this class
+    $("body").addClass("game-over");
+
+    // remove class after 200ms
+
+    setTimeout(() => {
+      $("body").removeClass("game-over");
+    }, "200");
+
+    // restart game
+    restartGame();
+  }
+}
+
+// When user click button
 
 $(".btn").click(function (e) {
-  let userChosenColor = $(this).attr("id");
+  // storing user chosen color
+  const userChosenColor = this.id;
+  console.log(userChosenColor, "user chosen color");
+  // storing color pattern
   userClickedPattern.push(userChosenColor);
-  userSound(userChosenColor);
-  console.log(userClickedPattern);
+  console.log("userchosenpattern", userClickedPattern);
+
+  // flash color when user click button
+  flashColor(userChosenColor);
+
+  // Making Sound when user click Sound
+  playSound(`${userChosenColor}.mp3`);
+
+  // Adding press class to button
   animatePress(userChosenColor);
+
+  // call checkAnswer with userChosenAnswer
+  checkAnswer(userClickedPattern.length - 1);
 });
